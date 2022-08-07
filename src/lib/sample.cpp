c8d3257a82cff6f250dcd9315c0462abcb6e8e1f
matsudai <matsudai.social.1131@gmail.com>
2022-08-07 04:19:29 +0000

    Update clang-format
    * "public:" indent -1 -> -2
    * namespace indent 0 -> 2

--- src/lib/sample.cpp
+++ src/lib/sample.cpp
@@ -1,5 +1,3 @@
 #include "sample.hpp"
 
-const std::string MyLib::Sample::to_s() const {
-  return "hogehoge";
-}
+const std::string MyLib::Sample::to_s() const { return "hogehoge"; }
--- src/lib/sample.hpp
+++ src/lib/sample.hpp
@@ -8,13 +8,13 @@ namespace MyLib {
   public:
     Sample() = default;
     Sample(const Sample&) = default;
-    Sample& operator= (const Sample&) = default;
+    Sample& operator=(const Sample&) = default;
     Sample(Sample&&) = default;
-    Sample& operator= (Sample&&) = default;
+    Sample& operator=(Sample&&) = default;
     virtual ~Sample() = default;
 
     const std::string to_s() const;
   };
-}
+}  // namespace MyLib
 
 #endif
